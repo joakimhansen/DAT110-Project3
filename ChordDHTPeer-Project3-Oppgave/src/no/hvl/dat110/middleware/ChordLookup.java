7a4b1caa53dd17c8fad826538f5c6f3c72a822fe
Joakim Hansen <42582449+571164@users.noreply.github.com>
2020-04-06 14:13:13 +0200

    task 1-5

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/middleware/ChordLookup.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/middleware/ChordLookup.java
@@ -5,6 +5,7 @@ package no.hvl.dat110.middleware;
 
 import java.math.BigInteger;
 import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -20,148 +21,196 @@ import no.hvl.dat110.util.Util;
 public class ChordLookup {
 
 	private Node node;
-	
+
 	public ChordLookup(Node node) {
 		this.node = node;
 	}
-	
+
 	public NodeInterface findSuccessor(BigInteger key) throws RemoteException {
-		
+
 		// ask this node to find the successor of key
-		
 		// get the successor of the node
-		
+		NodeInterface succ = this.node.getSuccessor();
+
 		// get the stub for this successor (Util.getProcessStub())
-		
-		// check that key is a member of the set {nodeid+1,...,succID} i.e. (nodeid+1 <= key <= succID) using the ComputeLogic
-		
-		// if logic returns true, then return the successor
-		
-		// if logic returns false; call findHighestPredecessor(key)
-		
-		// do return highest_pred.findSuccessor(key) - This is a recursive call until logic returns true
-				
-		return null;					
+		String name = succ.getNodeName();
+		int port = succ.getPort();
+		NodeInterface succStub = Util.getProcessStub(name, port);
+
+		if (succStub != null) {
+
+			// check that key is a member of the set {nodeid+1,...,succID} i.e. (nodeid+1 <=
+			// key <= succID) using the ComputeLogic
+			Boolean cond = Util.computeLogic(key, this.node.getNodeID().add(new BigInteger("1")), succStub.getNodeID());
+
+			// if logic returns true, then return the successor
+			if (cond) {
+				return succStub;
+			} else {
+				// do highest_pred.findSuccessor(key) - This is a recursive call until logic
+				// returns true
+				NodeInterface highestPred = findHighestPredecessor(key);
+				return highestPred.findSuccessor(key);
+			}
+
+		}
+
+		return null;
 	}
-	
+
 	/**
 	 * This method makes a remote call. Invoked from a local client
+	 * 
 	 * @param ID BigInteger
 	 * @return
 	 * @throws RemoteException
 	 */
 	private NodeInterface findHighestPredecessor(BigInteger key) throws RemoteException {
-		
+
+		BigInteger nodeID = this.node.getNodeID();
+
 		// collect the entries in the finger table for this node
-		
+		List<NodeInterface> fingerTable = this.node.getFingerTable();
+
 		// starting from the last entry, iterate over the finger table
-		
-		// for each finger, obtain a stub from the registry
-		
-		// check that finger is a member of the set {nodeID+1,...,ID-1} i.e. (nodeID+1 <= finger <= key-1) using the ComputeLogic
-		
-		// if logic returns true, then return the finger (means finger is the closest to key)
-		
-		return (NodeInterface) node;			
+		for (int i = fingerTable.size() - 1; i >= 0; i--) {
+
+			// for each finger, obtain a stub from the registry
+			NodeInterface finger = fingerTable.get(i);
+			NodeInterface fingerStub = Util.getProcessStub(finger.getNodeName(), finger.getPort());
+
+			// check that finger is a member of the set {nodeID+1,...,ID-1} i.e. (nodeID+1
+			// <= finger <= key-1) using the ComputeLogic
+			boolean cond = Util.computeLogic(fingerStub.getNodeID(), nodeID.add(new BigInteger("1")),
+					key.subtract(new BigInteger("1")));
+
+			// if logic returns true, then return the finger (means finger is the closest to
+			// key)
+			if (cond) {
+				return fingerStub;
+			}
+
+		}
+
+		return (NodeInterface) this.node;
 	}
-	
+
 	public void copyKeysFromSuccessor(NodeInterface succ) {
-		
+
 		Set<BigInteger> filekeys;
 		try {
 			// if this node and succ are the same, don't do anything
-			if(succ.getNodeName().equals(node.getNodeName()))
+			if (succ.getNodeName().equals(node.getNodeName()))
 				return;
-			
-			System.out.println("copy file keys that are <= "+node.getNodeName()+" from successor "+ succ.getNodeName()+" to "+node.getNodeName());
-			
+
+			System.out.println("copy file keys that are <= " + node.getNodeName() + " from successor "
+					+ succ.getNodeName() + " to " + node.getNodeName());
+
 			filekeys = new HashSet<>(succ.getNodeKeys());
 			BigInteger nodeID = node.getNodeID();
 			BigInteger succID = succ.getNodeID();
-			
-			for(BigInteger fileID : filekeys) {
-				// a small modification here if node > succ. We need to make sure the keys copied are only lower than succ
-				if(succ.getNodeID().compareTo(nodeID) == -1) {
-					if(fileID.compareTo(succID) == -1 || fileID.compareTo(succID) == 0) {
+
+			for (BigInteger fileID : filekeys) {
+				// a small modification here if node > succ. We need to make sure the keys
+				// copied are only lower than succ
+				if (succ.getNodeID().compareTo(nodeID) == -1) {
+					if (fileID.compareTo(succID) == -1 || fileID.compareTo(succID) == 0) {
 						BigInteger addresssize = Hash.addressSize();
 						fileID = fileID.add(addresssize);
 					}
 				}
 				// if fileID <= nodeID, copy the file to the newly joined node.
-				if(fileID.compareTo(nodeID) == -1 || fileID.compareTo(nodeID) == 0) {
-					System.out.println("fileID="+fileID+" | nodeID= "+nodeID);
-					node.addKey(fileID); 															// re-assign file to this successor node
-					Message msg = succ.getFilesMetadata().get(fileID);				
-					node.saveFileContent(msg.getNameOfFile(), fileID, msg.getBytesOfFile(), msg.isPrimaryServer()); 			// save the file in memory of the newly joined node
-					succ.removeKey(fileID); 	 																				// remove the file key from the successor
-					succ.getFilesMetadata().remove(fileID); 																	// also remove the saved file from memory
+				if (fileID.compareTo(nodeID) == -1 || fileID.compareTo(nodeID) == 0) {
+					System.out.println("fileID=" + fileID + " | nodeID= " + nodeID);
+					node.addKey(fileID); // re-assign file to this successor node
+					Message msg = succ.getFilesMetadata().get(fileID);
+					node.saveFileContent(msg.getNameOfFile(), fileID, msg.getBytesOfFile(), msg.isPrimaryServer()); // save
+																													// the
+																													// file
+																													// in
+																													// memory
+																													// of
+																													// the
+																													// newly
+																													// joined
+																													// node
+					succ.removeKey(fileID); // remove the file key from the successor
+					succ.getFilesMetadata().remove(fileID); // also remove the saved file from memory
 				}
 			}
-			
-			System.out.println("Finished copying file keys from successor "+ succ.getNodeName()+" to "+node.getNodeName());
+
+			System.out.println(
+					"Finished copying file keys from successor " + succ.getNodeName() + " to " + node.getNodeName());
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void notify(NodeInterface pred_new) throws RemoteException {
-		
+
 		NodeInterface pred_old = node.getPredecessor();
-		
+
 		// if the predecessor is null accept the new predecessor
-		if(pred_old == null) {
-			node.setPredecessor(pred_new);		// accept the new predecessor
+		if (pred_old == null) {
+			node.setPredecessor(pred_new); // accept the new predecessor
 			return;
 		}
-		
-		else if(pred_new.getNodeName().equals(node.getNodeName())) {
+
+		else if (pred_new.getNodeName().equals(node.getNodeName())) {
 			node.setPredecessor(null);
 			return;
 		} else {
 			BigInteger nodeID = node.getNodeID();
 			BigInteger pred_oldID = pred_old.getNodeID();
-			
+
 			BigInteger pred_newID = pred_new.getNodeID();
-			
-			// check that pred_new is between pred_old and this node, accept pred_new as the new predecessor
+
+			// check that pred_new is between pred_old and this node, accept pred_new as the
+			// new predecessor
 			// check that ftsuccID is a member of the set {nodeID+1,...,ID-1}
-			boolean cond = Util.computeLogic(pred_newID, pred_oldID.add(new BigInteger("1")), nodeID.add(new BigInteger("1")));
-			if(cond) {		
-				node.setPredecessor(pred_new);		// accept the new predecessor
-			}	
-		}		
+			boolean cond = Util.computeLogic(pred_newID, pred_oldID.add(new BigInteger("1")),
+					nodeID.add(new BigInteger("1")));
+			if (cond) {
+				node.setPredecessor(pred_new); // accept the new predecessor
+			}
+		}
 	}
-	
+
 	public void leaveRing() throws RemoteException {
-		
+
 		System.out.println("Attempting to update successor and predecessor before leaving the ring...");
-		
+
 		try {
-		 
-			NodeInterface prednode = node.getPredecessor();														// get the predecessor			
-			NodeInterface succnode = node.getSuccessor();														// get the successor		
-			NodeInterface prednodestub = Util.getProcessStub(prednode.getNodeName(), prednode.getPort());		// get the prednode stub			
-			NodeInterface succnodestub = Util.getProcessStub(succnode.getNodeName(), succnode.getPort());		// get the succnode stub			
-			Set<BigInteger> keyids = node.getNodeKeys();									// get the keys for chordnode
-						 
-			if(succnodestub != null) {												// add chordnode's keys to its successor
+
+			NodeInterface prednode = node.getPredecessor(); // get the predecessor
+			NodeInterface succnode = node.getSuccessor(); // get the successor
+			NodeInterface prednodestub = Util.getProcessStub(prednode.getNodeName(), prednode.getPort()); // get the
+																											// prednode
+																											// stub
+			NodeInterface succnodestub = Util.getProcessStub(succnode.getNodeName(), succnode.getPort()); // get the
+																											// succnode
+																											// stub
+			Set<BigInteger> keyids = node.getNodeKeys(); // get the keys for chordnode
+
+			if (succnodestub != null) { // add chordnode's keys to its successor
 				keyids.forEach(fileID -> {
 					try {
-						System.out.println("Adding fileID = "+fileID+" to "+succnodestub.getNodeName());
+						System.out.println("Adding fileID = " + fileID + " to " + succnodestub.getNodeName());
 						succnodestub.addKey(fileID);
-						Message msg = node.getFilesMetadata().get(fileID);				
-						succnodestub.saveFileContent(msg.getNameOfFile(), fileID, msg.getBytesOfFile(), msg.isPrimaryServer()); 			// save the file in memory of the newly joined node
+						Message msg = node.getFilesMetadata().get(fileID);
+						succnodestub.saveFileContent(msg.getNameOfFile(), fileID, msg.getBytesOfFile(),
+								msg.isPrimaryServer()); // save the file in memory of the newly joined node
 					} catch (RemoteException e) {
-						//e.printStackTrace();
-					} 
+						// e.printStackTrace();
+					}
 				});
 
-				succnodestub.setPredecessor(prednodestub); 							// set prednode as the predecessor of succnode
+				succnodestub.setPredecessor(prednodestub); // set prednode as the predecessor of succnode
 			}
-			if(prednodestub != null) {
-				prednodestub.setSuccessor(succnodestub);							// set succnode as the successor of prednode			
-			} 
-		}catch(Exception e) {
+			if (prednodestub != null) {
+				prednodestub.setSuccessor(succnodestub); // set succnode as the successor of prednode
+			}
+		} catch (Exception e) {
 			//
 			System.out.println("some errors while updating succ/pred/keys...");
 		}
--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/FileManager.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/FileManager.java
@@ -1,6 +1,5 @@
 package no.hvl.dat110.util;
 
-
 /**
  * @author tdoy
  * dat110 - project 3
@@ -18,170 +17,209 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
+import no.hvl.dat110.middleware.ChordLookup;
 import no.hvl.dat110.middleware.Message;
+import no.hvl.dat110.middleware.Node;
 import no.hvl.dat110.rpc.interfaces.NodeInterface;
 import no.hvl.dat110.util.Hash;
 
 public class FileManager {
-	
-	private BigInteger[] replicafiles;							// array stores replicated files for distribution to matching nodes
-	private int numReplicas;									// let's assume each node manages nfiles (5 for now) - can be changed from the constructor
+
+	private BigInteger[] replicafiles; // array stores replicated files for distribution to matching nodes
+	private int numReplicas; // let's assume each node manages nfiles (5 for now) - can be changed from the
+								// constructor
 	private NodeInterface chordnode;
-	private String filepath; 									// absolute filepath
-	private String filename;									// only filename without path and extension
+	private String filepath; // absolute filepath
+	private String filename; // only filename without path and extension
 	private BigInteger hash;
 	private byte[] bytesOfFile;
 	private String sizeOfByte;
-	
+
 	private Set<Message> activeNodesforFile = null;
-	
+
 	public FileManager(NodeInterface chordnode) throws RemoteException {
 		this.chordnode = chordnode;
 	}
-	
+
 	public FileManager(NodeInterface chordnode, int N) throws RemoteException {
 		this.numReplicas = N;
 		replicafiles = new BigInteger[N];
 		this.chordnode = chordnode;
 	}
-	
+
 	public FileManager(NodeInterface chordnode, String filepath, int N) throws RemoteException {
 		this.filepath = filepath;
 		this.numReplicas = N;
 		replicafiles = new BigInteger[N];
 		this.chordnode = chordnode;
 	}
-	
+
 	public void createReplicaFiles() {
-	 	
+
 		// implement
-		
+
 		// set a loop where size = numReplicas
-		
 		// replicate by adding the index to filename
-		
 		// hash the replica
-		
 		// store the hash in the replicafiles array.
+		for (int i = 0; i < numReplicas; i++) {
+			replicafiles[i] = Hash.hashOf(filename + i);
+		}
 
 	}
-	
-    /**
-     * 
-     * @param bytesOfFile
-     * @throws RemoteException 
-     */
-    public int distributeReplicastoPeers() throws RemoteException {
-    	int counter = 0;
-    	
-    	// Task1: Given a filename, make replicas and distribute them to all active peers such that: pred < replica <= peer
-    	
-    	// Task2: assign a replica as the primary for this file. Hint, see the slide (project 3) on Canvas
-    	
-    	// create replicas of the filename
-    	
+
+	/**
+	 * 
+	 * @param bytesOfFile
+	 * @throws RemoteException
+	 */
+	public int distributeReplicastoPeers() throws RemoteException {
+		int counter = 0;
+
+		// Task1: Given a filename, make replicas and distribute them to all active
+		// peers such that: pred < replica <= peer
+
+		// Task2: assign a replica as the primary for this file. Hint, see the slide
+		// (project 3) on Canvas
+
+		// randomly appoint the primary server to this file replicas
+		Random rnd = new Random();
+		int index = rnd.nextInt(Util.numReplicas - 1);
+
+		// create replicas of the filename
+		createReplicaFiles();
+
 		// iterate over the replicas
-    	
-    	// for each replica, find its successor by performing findSuccessor(replica)
-    	
-    	// call the addKey on the successor and add the replica
-    	
-    	// call the saveFileContent() on the successor
-    	
-    	// increment counter
-    	
-    		
+		for (int i = 0; i < replicafiles.length; i++) {
+
+			// fileID for replica
+			BigInteger fileID = replicafiles[i];
+
+			// for each replica, find its successor by performing findSuccessor(replica)
+			NodeInterface successor = chordnode.findSuccessor(fileID);
+
+			// call the addKey on the successor and add the replica
+			successor.addKey(fileID);
+
+			// call the saveFileContent() on the successor
+			// server at index=counter will be our primary for this file
+			if (counter == index) {
+				successor.saveFileContent(filename, fileID, bytesOfFile, true);
+			} else {
+				successor.saveFileContent(filename, fileID, bytesOfFile, false);
+			}
+
+			// increment counter
+			counter++;
+		}
+
 		return counter;
-    }
-	
+	}
+
 	/**
 	 * 
 	 * @param filename
 	 * @return list of active nodes having the replicas of this file
-	 * @throws RemoteException 
+	 * @throws RemoteException
 	 */
 	public Set<Message> requestActiveNodesForFile(String filename) throws RemoteException {
-		
+
 		this.filename = filename;
 		Set<Message> succinfo = new HashSet<Message>();
+
 		// Task: Given a filename, find all the peers that hold a copy of this file
-		
+
 		// generate the N replicas from the filename by calling createReplicaFiles()
-		
+		createReplicaFiles();
+
 		// it means, iterate over the replicas of the file
-		
-		// for each replica, do findSuccessor(replica) that returns successor s.
-		
-		// get the metadata (Message) of the replica from the successor, s (i.e. active peer) of the file
-		
-		// save the metadata in the set succinfo.
-		
+		for (int i = 0; i < replicafiles.length; i++) {
+
+			BigInteger fileID = replicafiles[i];
+
+			// for each replica, do findSuccessor(replica) that returns successor s.
+			NodeInterface successor = chordnode.findSuccessor(fileID);
+
+			// get the metadata (Message) of the replica from the successor, s (i.e. active
+			// peer) of the file
+			Message metadata = successor.getFilesMetadata(fileID);
+
+			// save the metadata in the set succinfo.
+			succinfo.add(metadata);
+
+		}
+
 		this.activeNodesforFile = succinfo;
-		
+
 		return succinfo;
 	}
-	
+
 	/**
 	 * Find the primary server - Remote-Write Protocol
-	 * @return 
+	 * 
+	 * @return
 	 */
 	public NodeInterface findPrimaryOfItem() {
 
-		// Task: Given all the active peers of a file (activeNodesforFile()), find which is holding the primary copy
-		
+		// Task: Given all the active peers of a file (activeNodesforFile()), find which
+		// is holding the primary copy
+
 		// iterate over the activeNodesforFile
-		
+
 		// for each active peer (saved as Message)
-		
-		// use the primaryServer boolean variable contained in the Message class to check if it is the primary or not
-		
+
+		// use the primaryServer boolean variable contained in the Message class to
+		// check if it is the primary or not
+
 		// return the primary
-		
-		return null; 
+
+		return null;
 	}
-	
-    /**
-     * Read the content of a file and return the bytes
-     * @throws IOException 
-     * @throws NoSuchAlgorithmException 
-     */
-    public void readFile() throws IOException, NoSuchAlgorithmException {
-    	
-    	File f = new File(filepath);
-    	
-    	byte[] bytesOfFile = new byte[(int) f.length()];
-    	
+
+	/**
+	 * Read the content of a file and return the bytes
+	 * 
+	 * @throws IOException
+	 * @throws NoSuchAlgorithmException
+	 */
+	public void readFile() throws IOException, NoSuchAlgorithmException {
+
+		File f = new File(filepath);
+
+		byte[] bytesOfFile = new byte[(int) f.length()];
+
 		FileInputStream fis = new FileInputStream(f);
-        
-        fis.read(bytesOfFile);
+
+		fis.read(bytesOfFile);
 		fis.close();
-		
-		//set the values
-		filename = f.getName().replace(".txt", "");		
+
+		// set the values
+		filename = f.getName().replace(".txt", "");
 		hash = Hash.hashOf(filename);
 		this.bytesOfFile = bytesOfFile;
-		double size = (double) bytesOfFile.length/1000;
+		double size = (double) bytesOfFile.length / 1000;
 		NumberFormat nf = new DecimalFormat();
 		nf.setMaximumFractionDigits(3);
 		sizeOfByte = nf.format(size);
-		
-		System.out.println("filename="+filename+" size="+sizeOfByte);
-    	
-    }
-    
-    public void printActivePeers() {
-    	
-    	activeNodesforFile.forEach(m -> {
-    		String peer = m.getNodeIP();
-    		String id = m.getNodeID().toString();
-    		String name = m.getNameOfFile();
-    		String hash = m.getHashOfFile().toString();
-    		int size = m.getBytesOfFile().length;
-    		
-    		System.out.println(peer+": ID = "+id+" | filename = "+name+" | HashOfFile = "+hash+" | size ="+size);
-    		
-    	});
-    }
+
+		System.out.println("filename=" + filename + " size=" + sizeOfByte);
+
+	}
+
+	public void printActivePeers() {
+
+		activeNodesforFile.forEach(m -> {
+			String peer = m.getNodeIP();
+			String id = m.getNodeID().toString();
+			String name = m.getNameOfFile();
+			String hash = m.getHashOfFile().toString();
+			int size = m.getBytesOfFile().length;
+
+			System.out.println(
+					peer + ": ID = " + id + " | filename = " + name + " | HashOfFile = " + hash + " | size =" + size);
+
+		});
+	}
 
 	/**
 	 * @return the numReplicas
@@ -189,49 +227,56 @@ public class FileManager {
 	public int getNumReplicas() {
 		return numReplicas;
 	}
-	
+
 	/**
 	 * @return the filename
 	 */
 	public String getFilename() {
 		return filename;
 	}
+
 	/**
 	 * @param filename the filename to set
 	 */
 	public void setFilename(String filename) {
 		this.filename = filename;
 	}
+
 	/**
 	 * @return the hash
 	 */
 	public BigInteger getHash() {
 		return hash;
 	}
+
 	/**
 	 * @param hash the hash to set
 	 */
 	public void setHash(BigInteger hash) {
 		this.hash = hash;
 	}
+
 	/**
 	 * @return the bytesOfFile
-	 */ 
+	 */
 	public byte[] getBytesOfFile() {
 		return bytesOfFile;
 	}
+
 	/**
 	 * @param bytesOfFile the bytesOfFile to set
 	 */
 	public void setBytesOfFile(byte[] bytesOfFile) {
 		this.bytesOfFile = bytesOfFile;
 	}
+
 	/**
 	 * @return the size
 	 */
 	public String getSizeOfByte() {
 		return sizeOfByte;
 	}
+
 	/**
 	 * @param size the size to set
 	 */
